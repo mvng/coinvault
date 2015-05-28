d028907b00c71466bf9ec591e1daf06f4dac0806
mattvn <matthew.nguyen@me.com>
2015-05-27 17:38:16 -0700

    Learning CRUD

---
+++ js/crud.js
@@ -0,0 +1,10 @@
+//Objects to edit in Parse
+//Type, unitPrice, premium Total createdAt
+Parse.initialize("uuMKgtd7piSYYS1OIicbKekLesPHKKerHp21X3Zk", "J10xkBbtYfn4EPD9TQzp0BNJI7sQ2UXmJqY0Dkgu");
+
+
+var TestObject = Parse.Object.extend("TestObject");
+var testObject = new TestObject();
+testObject.save({foo: "bar"}).then(function(object) {
+  alert("yay! it worked");
+});
