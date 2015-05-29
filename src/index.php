d613f32431798963622a35022b97df1f2ac593aa
dpark213 <dustinpark@Dustins-MacBook-Pro-2.local>
2015-05-28 17:58:51 -0700

    25
    df

--- src/index.php
+++ src/index.php
@@ -10,7 +10,7 @@ $i = 0;
 		$br_label_arr[] = date('M j',strtotime($br_data[0])); //pull dates
 		$br_value_arr[] = $br_data[1]; //pull prices
 
-		if (++$i == 40) break;
+		if (++$i == 20) break;
 	}
 $br_labels = array_reverse($br_label_arr); //reverse the data for ASC
 $br_values = array_reverse($br_value_arr); //reverse the data for ASC
