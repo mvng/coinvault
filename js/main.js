c3ed827f6ae3a800ade39f499d13b15e3d020e9d
Jacob Terrado <jsterrad@ucsd.edu>
2015-05-29 03:47:31 -0700

    Linked index.html to src files. Changed coinflip to coin vault

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ function loadTopNav() {
 	document.write("            <\/symbol>");
 	document.write("            <use xlink:href=\"#icon-spinner2\"><\/use>");
 	document.write("        <\/svg>");
-	document.write("        <a href=\"dashboard.html\">COINFLIP<\/a>");
+	document.write("        <a href=\"dashboard.html\">COIN VAULT<\/a>");
 	document.write("        <svg class=\"icon-cog\">");
 	document.write("            <symbol id=\"icon-cog\" viewBox=\"0 0 1024 1024\">");
 	document.write("                <title>cog<\/title>");
@@ -27,7 +27,7 @@ function loadTopNavPersist() {
 	document.write("            <\/symbol>");
 	document.write("            <use xlink:href=\"#icon-spinner2\"><\/use>");
 	document.write("        <\/svg>");
-	document.write("        <a href=\"dashboard.html\">COINFLIP<\/a>");
+	document.write("        <a href=\"dashboard.html\">COIN VAULT<\/a>");
 	document.write("        <svg class=\"icon-cog\">");
 	document.write("            <symbol id=\"icon-cog\" viewBox=\"0 0 1024 1024\">");
 	document.write("                <title>cog<\/title>");
@@ -89,7 +89,7 @@ function loadSideNav(selected){
 
 function loadFooter(){
 	document.write("    <footer>");
-	document.write("        &copy; 2015 CoinFlip");
+	document.write("        &copy; 2015 Coin Vault");
 	document.write("    <\/footer> ");
 }
 
