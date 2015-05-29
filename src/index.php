05e63d597e5dac4aeed6d329d42385a809f999d3
dpark213 <dustinpark@Dustins-MacBook-Pro-2.local>
2015-05-28 17:29:52 -0700

    crap

--- src/index.php
+++ src/index.php
@@ -9,7 +9,7 @@ $i = 0;
 	foreach ($br_obj['data'] as $br_data){ //loop through data
 		$br_label_arr[] = date('M j',strtotime($br_data[0])); //pull dates
 		$br_value_arr[] = $br_data[6]; //pull prices
-		if (++$i == 100) break;
+		if (++$i == 30) break;
 	}
 $br_labels = array_reverse($br_label_arr); //reverse the data for ASC
 $br_values = array_reverse($br_value_arr); //reverse the data for ASC
